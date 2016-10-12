d66a23b27216e3b964da2b9a091a74ad8c2a3b40
Long Ang <aisk1988@gmail.com>
2016-10-12 15:01:04 +0800

    add chrysanthemum.Println and chrysanthemum.Printf

--- chrysanthemum.go
+++ chrysanthemum.go
@@ -10,26 +10,39 @@ import (
 
 var isTerminal = isatty.IsTerminal(os.Stdout.Fd())
 
-var Frames = []string{
-	color.MagentaString("⠋"),
-	color.MagentaString("⠙"),
-	color.MagentaString("⠹"),
-	color.MagentaString("⠸"),
-	color.MagentaString("⠼"),
-	color.MagentaString("⠴"),
-	color.MagentaString("⠦"),
-	color.MagentaString("⠧"),
-	color.MagentaString("⠇"),
-	color.MagentaString("⠏"),
-}
-var Success = color.GreenString("✓")
-var Fail = color.RedString("✗")
+// Frames is spinner's frame. replace it with what you like
+var Frames []string
 
+// Success is the flag symbol after you called the Successed
+var Success string
+
+// Fail is the flag symbol after you called the Failed
+var Fail string
+
+// Chrysanthemum represent a spinner instance
 type Chrysanthemum struct {
 	stop    chan bool
 	stopped bool
 }
 
+func init() {
+	Frames = []string{
+		color.MagentaString("⠋"),
+		color.MagentaString("⠙"),
+		color.MagentaString("⠹"),
+		color.MagentaString("⠸"),
+		color.MagentaString("⠼"),
+		color.MagentaString("⠴"),
+		color.MagentaString("⠦"),
+		color.MagentaString("⠧"),
+		color.MagentaString("⠇"),
+		color.MagentaString("⠏"),
+	}
+	Success = color.GreenString("✓")
+	Fail = color.RedString("✗")
+}
+
+// New create a spinner instance
 func New(text string) *Chrysanthemum {
 	if !isTerminal {
 		fmt.Print(text)
@@ -42,6 +55,7 @@ func New(text string) *Chrysanthemum {
 	}
 }
 
+// Start will let your chrysanthemum spin!
 func (c *Chrysanthemum) Start() *Chrysanthemum {
 	if !isTerminal {
 		return c
@@ -52,17 +66,17 @@ func (c *Chrysanthemum) Start() *Chrysanthemum {
 	i := 0
 	go func() {
 		for {
+			if i == len(Frames) {
+				i = 0
+			}
+			fmt.Printf("\r %s ", Frames[i])
+			i++
 			select {
 			case <-c.stop:
 				return
 			default:
 				time.Sleep(100 * time.Millisecond)
 			}
-			if i == len(Frames) {
-				i = 0
-			}
-			fmt.Printf("\r %s ", Frames[i])
-			i++
 		}
 	}()
 
@@ -94,4 +108,18 @@ func (c *Chrysanthemum) Failed() {
 
 func (c *Chrysanthemum) End() {
 	c.Successed()
+}
+
+func Printf(format string, args ...interface{}) {
+	if isTerminal {
+		format = " " + Success + " " + format
+	}
+	fmt.Printf(format, args...)
+}
+
+func Println(args ...interface{}) {
+	if isTerminal {
+		args = append([]interface{}{" " + Success}, args...)
+	}
+	fmt.Println(args...)
 }
